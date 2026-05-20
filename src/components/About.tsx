37fae1b0f138698c9b7fb43da9742f86e92f2902
kartiktnj <kartik.taneja1409@gmail.com>
2026-05-21 02:01:12 +0530

    initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          As a Full-Stack Developer, I thrive on exploring the frontiers of both frontend and backend development. I'm passionate about adopting new technologies and refining my coding skills.
+          </p>
+        <p className="para">
+          Beyond coding, you'll find me on the football field, trekking through nature, or lost in the melodies of diverse music genres.
+          </p>
+        <em className="para">Let's code together or kick back for a game of football sometime!</em>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.tsx
@@ -0,0 +1,72 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Connect</h4>
+            <p>
+              <a
+                href="https://www.linkedin.com/in/kartiktnj/"
+                target="_blank"
+                rel="noreferrer"
+                data-cursor="disable"
+              >
+                LinkedIn — kartiktnj
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>
+              B.Tech Information Technology, Manipal University —
+              2017-2021
+            </p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/kartiktnj"
+              target="_blank"
+              rel="noreferrer"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              GitHub <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kartiktnj/"
+              target="_blank"
+              rel="noreferrer"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              LinkedIn <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/whattheferb/"
+              target="_blank"
+              rel="noreferrer"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Kartik Taneja</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
